ac955f8dfc66bc9e475c0b81eaaed752b9b9a335
LingGrani <luthfiangger2510@gmail.com>
2024-10-31 07:49:58 +0700

    Menambahkan method Post, Delete, dan Put

--- routes/todo.js
+++ routes/todo.js
@@ -14,4 +14,32 @@ router.get('/', (req, res) => {
     res.json(todo);
 });
 
+router.post('/', (req, res) => {
+  const newTodo = {
+    id: todo.length + 1,
+    task: req.body.task,
+    completed: false
+  };
+  todo.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
 module.exports = router;
+
+router.delete('/:id', (req, res) => {
+    const todoIndex = todo.findIndex(t => t.id === parseInt(req.params.id));
+    if (todoIndex === -1) return res.status(404).json({ message: 'Tugas tidak ditemukan' });
+
+    const deletedTodo = todo.splice(todoIndex, 1)[0];
+    res.status(200).json({ message: `Tugas '${deletedTodo.task}' telah dihapus` });
+});
+
+router.put('/:id', (req, res) =>{
+    const todos = todo.find(t => t.id === parseInt(req.params.id));
+    if (!todos) return res.status(404).json({message: 'Tugas tidak ditemukan'});
+    todos.task = req.body.task || todos.task;
+    res.status(200).json ({
+        message: `tugas dengan ID ${todos.id} telah diperbarui`,
+        updateTodo: todo
+    })
+})
